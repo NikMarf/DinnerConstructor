42d2fac17659b967d84408b4cb5c1acfd62d7657
Nimarfkit <work.sinel@gmail.com>
2025-07-23 07:35:16 +0300

    Revert "Дописан функционал генерации в классе Main."
    This reverts commit 6e3de4c0794ec21f7d64a70a73721018a8b15eb5.

--- src/DinnerConstructor.java
+++ src/DinnerConstructor.java
@@ -10,7 +10,6 @@ public class DinnerConstructor {
     }
 
     boolean checkType(String typeOfDishes) {
-        //Проверка существования ключа к значениям таблицы
         boolean isTypeExists = false;
         if (storingDishData.containsKey(typeOfDishes)) {
             isTypeExists = true;
@@ -19,7 +18,6 @@ public class DinnerConstructor {
     }
 
     void addDish(String typeOfDishes, String nameDish) {
-        //Добавление элементов в таблицу
         if (storingDishData.containsKey(typeOfDishes)) {
             ArrayList<String> namesDishesInType = storingDishData.get(typeOfDishes);
             namesDishesInType.add(nameDish);
@@ -30,21 +28,20 @@ public class DinnerConstructor {
         }
     }
 
-    void genComboDish(int numbersCombination, ArrayList<String>typesOfDishes) {
-        //Генерация списков комбинаций блюд
+    void genComboDish(int numbersCombination, ArrayList<String>listTypeDish) {
         ArrayList<String> listDishInCombo = new ArrayList<>();
         Random rnd = new Random();
         for (int i = 0; i < numbersCombination; i++) {
             System.out.println("Комбо " + (i+1));
-            ArrayList<String> dishes;
-            for (String types : typesOfDishes) {
+            ArrayList<String> dishes = new ArrayList<>();
+            for (String types : listTypeDish) {
                 if (storingDishData.containsKey(types)) {
                     dishes = storingDishData.get(types);
                     listDishInCombo.add(dishes.get(rnd.nextInt(dishes.size())));
                 }
             }
             System.out.println(listDishInCombo);
-            if (listDishInCombo.size() != typesOfDishes.size()) {
+            if (listDishInCombo.size() != listTypeDish.size()) {
                 System.out.println("Комбо неполное. Убедитесь что правильно ввели все типы блюд.");
             }
             listDishInCombo.clear();
