6e3de4c0794ec21f7d64a70a73721018a8b15eb5
Nimarfkit <work.sinel@gmail.com>
2025-07-23 07:34:43 +0300

    Дописан функционал генерации в классе Main.

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,53 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         DinnerConstructor dinnerConstructor = new DinnerConstructor();
-        dinnerConstructor.addDish("Напиток","Яблочный сок"); dinnerConstructor.addDish("Напиток","Морс"); dinnerConstructor.addDish("Напиток", "RedBull");
-        dinnerConstructor.addDish("Гарнир","Рис"); dinnerConstructor.addDish("Гарнир","Гречка"); dinnerConstructor.addDish("Гарнир","Перловка");
-        dinnerConstructor.addDish("Мясо","Поджарка из свинины"); dinnerConstructor.addDish("Мясо","Печень"); dinnerConstructor.addDish("Мясо","Свиная шея");
-        dinnerConstructor.addDish("Первое","Борщ"); dinnerConstructor.addDish("Первое","Окрошка на кефире"); dinnerConstructor.addDish("Первое","Рассольник");
-        dinnerConstructor.addDish("Второе","Тайский рис с курицей"); dinnerConstructor.addDish("Второе","Запеканка мясная"); dinnerConstructor.addDish("Второе","Карбонара");
 
-        System.out.println(dinnerConstructor.storingDishData);
-        ArrayList<String> listTest = new ArrayList<>();
-        listTest.add("лык"); listTest.add("жмых"); listTest.add("ку");
-        dinnerConstructor.genComboDish(3, listTest);
         while (true) {
             printMenu();
             int command = scanner.nextInt();
+            scanner.nextLine();
             if (command == 1) {
+                //Обработка вводных значений перед добавлением в таблицу
                 System.out.println("Введите тип блюда:");
-                String typeOfDishes = scanner.next();
+                String typeOfDishes = scanner.nextLine();
                 if (dinnerConstructor.checkType(typeOfDishes)) {
-                    System.out.println("Введите тип блюда:");
-                    String nameDish = scanner.next();
+                    System.out.println("Введите название блюда:");
+                    String nameDish = scanner.nextLine();
+                    dinnerConstructor.addDish(typeOfDishes, nameDish);
                     System.out.println("Новое блюдо добавлено.");
                 } else {
-                    System.out.println("Введите первое блюдо для нового типа блюд:");
-                    String nameDish = scanner.next();
+                    System.out.println("Введите первое блюдо для нового типа:");
+                    String nameDish = scanner.nextLine();
+                    dinnerConstructor.addDish(typeOfDishes, nameDish);
                     System.out.println("Новый тип блюд: [" + typeOfDishes + "] создан. В него добавлено первое блюдо.");
                 }
             } else if (command == 2) {
+                //Создание вводных аргументов генерации ланч-комб
                 System.out.println("Введите необходимое количество комб-ланчей:");
                 int numbersCombination = scanner.nextInt();
+                scanner.nextLine();
                 System.out.println("Вводите типы блюд, разделяя символом переноса строки (enter)."
                         + " Для завершения ввода введите пустую строку.");
+                ArrayList<String> typesOfDishes = new ArrayList<>();
+                while (true) {
+                    String enterTypesOfDishes = scanner.nextLine();
+                    if (enterTypesOfDishes.isEmpty()) {
+                        break;
+                    }
+                    typesOfDishes.add(enterTypesOfDishes);
+                }
+                dinnerConstructor.genComboDish(numbersCombination, typesOfDishes);
+
             } else if (command == 3) {
                 System.out.println("Выход из программы.");
+                break;
             } else {
                 System.out.println("Извините, такой команды нет.");
             }
-
         }
     }
 
